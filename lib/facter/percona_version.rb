6a2e8f6e106af25410333a94fe472dafb46cfb5d
Freark van der Bos <safariman@gmail.com>
2014-04-08 10:12:36 +0200

    Checking version by checking a more common package

--- lib/facter/percona_version.rb
+++ lib/facter/percona_version.rb
@@ -2,6 +2,6 @@
 
 Facter.add("perconaversion") do
 	setcode do
-		Facter::Util::Resolution.exec('/usr/bin/dpkg-query -W -f=\'${Version}\' percona-xtradb-cluster-server-5.5 2>/dev/null')
+		Facter::Util::Resolution.exec('/usr/bin/dpkg-query -W -f=\'${Version}\' percona-xtradb-cluster-common-5.5 2>/dev/null')
 	end
 end
